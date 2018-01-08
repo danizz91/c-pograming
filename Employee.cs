fd232e5703f8b096157fc2de4a0c50036673b502
danizz91 <35202602+danizz91@users.noreply.github.com>
2018-01-08 09:18:08 +0200

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiProject.Models
+{
+    public class Employee
+    {
+        public enum EmployeeRole
+        {
+            Employee,
+            Manager
+        };
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public EmployeeRole Role { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public string UniqueId
+        {
+            get { return (this.FullName + "_" + this.Email).ToLower(); ; }
+        }
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string fullName, string email, string phoneNumber, EmployeeRole role, DateTime startDate)
+        {
+            if (String.IsNullOrEmpty(fullName) || String.IsNullOrEmpty(email))
+            {
+                throw new Exception("Missing FullName Or Email For Employee");
+            }
+
+            this.FullName = fullName;
+            this.Email = email;
+            this.PhoneNumber = phoneNumber;
+            this.Role = role;
+            this.StartDate = startDate;
+        }
+    }
+}
---
+++ EmployeeController.cs
@@ -0,0 +1,76 @@
+﻿using MultipartDataMediaFormatter.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Reflection;
+using WebApiProject.Models;
+using System.IO;
+using Newtonsoft.Json;
+namespace WebApiTest.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        List<Employee> employees = new List<Employee>();
+        public EmployeeController()
+        {
+            System.Globalization.CultureInfo.CurrentCulture.ClearCachedData();
+            employees.Add(new Employee("mor", "asd@gmail.com", "123123", Employee.EmployeeRole.Manager, DateTime.UtcNow));
+            employees.Add(new Employee("daniel", "daniel@gmail.com", "321", Employee.EmployeeRole.Employee, DateTime.UtcNow));
+            employees.Add(new Employee("shabi", "shabi@gmail.com", "123", Employee.EmployeeRole.Employee, DateTime.UtcNow.AddYears(-1)));
+        }
+
+        public IEnumerable<Employee> GetEmployessBySeniority()
+        {
+            DateTime lastYear = DateTime.UtcNow.AddYears(-1);
+            return GetAllEmployees().Where(x => x.StartDate != null && (x.StartDate <= lastYear)).ToList();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+           return employees;
+            
+        }
+
+        public IHttpActionResult GetEmployee(string id)
+        {
+            var Employee = GetAllEmployees().FirstOrDefault((e) => e.UniqueId == id);
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(Employee);
+        }
+
+
+ 
+
+
+        [HttpPost]
+        public object Post([FromBody]Employee data)
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        public void Delete(string id)
+        {
+            var Employee = GetAllEmployees().FirstOrDefault((e) => e.UniqueId == id);
+            GetAllEmployees().ToList().Remove(Employee);
+        }
+
+        public List<Employee> LoadJson()
+        {
+            string filePath = System.AppDomain.CurrentDomain.BaseDirectory + "employees.json";
+            using (StreamReader r = new StreamReader(filePath))
+            {
+                string json = r.ReadToEnd();
+                List<Employee> items = JsonConvert.DeserializeObject<List<Employee>>(json);
+                return items;
+            }
+        }
+
+    }
+}
